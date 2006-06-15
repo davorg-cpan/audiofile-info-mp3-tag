5aad62ea28f8102607e00a903a18ec44e0f526a4
Dave Cross <dave@dave.org.uk>
2006-06-15 20:40:50 +0000

    Version bump

--- lib/AudioFile/Info/MP3/Tag.pm
+++ lib/AudioFile/Info/MP3/Tag.pm
@@ -101,7 +101,7 @@ Dave Cross, E<lt>dave@dave.org.ukE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003 by Dave Cross
+Copyright 2003-2006 by Dave Cross
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
